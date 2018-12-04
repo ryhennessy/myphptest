70e1196a99d7c729bb84640fecb42d4cc3f9d0b4
Ryan Hennessy <rhenness@redhat.com>
2018-12-04 08:45:01 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
    This is a page where I test stuff
 <?php
    echo '<p>I was here!';
-   echo 'My username is ' .$_ENV["USER"] . '!';
+   echo '<p>My username is ' .$_ENV["USER"] . '!';
 ?>
 </HTML>
